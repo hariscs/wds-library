c6992ecd70d459a492cfb37999eecae40ee7be8a
Haris Shah <harisshah385@gmail.com>
2021-12-04 01:26:23 +0500

    author routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -20,6 +21,11 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
+// sending data
+// app.use(express.json());
+app.use(express.urlencoded({ limit: '10mb', extended: false }));
+
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
